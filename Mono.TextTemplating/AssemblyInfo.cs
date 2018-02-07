2250b0de8eea7d09de7401c955c2e0baae62dcd3
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2018-02-07 17:37:42 -0500

    Switch to sdk-style projects

--- Mono.TextTemplating/AssemblyInfo.cs
+++ Mono.TextTemplating/AssemblyInfo.cs
@@ -23,17 +23,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System;
 
-[assembly: AssemblyTitle("Mono.TextTemplating")]
-[assembly: AssemblyDescription("An implementation of Visual Studio's T4 text templating")]
-[assembly: AssemblyCompany("The Mono Project")]
-[assembly: AssemblyProduct("MonoDevelop")]
-[assembly: AssemblyCopyright("MIT/X11")]
-[assembly: CLSCompliant (true)]
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo ("Mono.TextTemplating.Tests,PublicKey=0024000004800000940000000602000000240000525341310004000001000100490b59506a03eb5fc5524722a6526eff804b7880a968a581a0bbb73cd6ab93c7b94a14150cb4ea40f610bebf607cea5e8a93ef25d124983300f4bdfe44859430a20a4ed2ac32cfd3a6f0aa12702df819b7799cc0fcf077eea706a27252d59a8a10e5164c2cdddd6680ca76b02ca244e83c1a8cc44b2691052b93ab30bcc613ad")]
-
-//[assembly: AssemblyVersion("1.0.0.0")]
