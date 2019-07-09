30def8f01caa0aa6fd4d6c99666286dacc99441b
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-07-09 19:05:54 -0400

    Rough out some build targets

---
+++ Mono.TextTemplating.Build/TextTransform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Mono.TextTemplating.Build
+{
+	// parameter values:
+	// VS TextTemplatingFileGenerator tool: all MSBuild variables from the project, but not T4ParameterValue items
+	// VS MSBuild targets: T4ParameterValue items, but not arbitrary MSBuild properties (ironically)
+	// maybe we should add some common ones by default?
+	public class TextTransform : Task
+	{
+		public string DefaultNamespace { get; set; }
+		public ITaskItem [] PreprocessTemplates { get; set; }
+		public ITaskItem [] TransformTemplates { get; set; }
+		public string IncludeFolders { get; set; }
+		public ITaskItem [] DirectiveProcessors { get; set; }
+		public ITaskItem [] AssemblyReferences { get; set; }
+		public ITaskItem [] ReferencePaths { get; set; }
+
+		public ITaskItem [] ParameterValues { get; set; }
+
+		public override bool Execute ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
