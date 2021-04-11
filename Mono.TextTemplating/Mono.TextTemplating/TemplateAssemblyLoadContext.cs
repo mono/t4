204314223b2500e31c3591eaebbabcfe05e77ce4
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2021-04-11 00:26:19 -0400

    Fix custom hosts w/ AssemblyLoadContext

--- Mono.TextTemplating/Mono.TextTemplating/TemplateAssemblyLoadContext.cs
+++ Mono.TextTemplating/Mono.TextTemplating/TemplateAssemblyLoadContext.cs
@@ -16,17 +16,21 @@ namespace Mono.TextTemplating
 	{
 		readonly string[] templateAssemblyFiles;
 		readonly ITextTemplatingEngineHost host;
+		readonly Assembly hostAssembly;
 
 		readonly AssemblyName codeDomAsmName;
 		readonly AssemblyName textTemplatingAsmName;
+		readonly AssemblyName hostAsmName;
 
 		public TemplateAssemblyLoadContext (string[] templateAssemblyFiles, ITextTemplatingEngineHost host)
 		{
 			this.templateAssemblyFiles = templateAssemblyFiles;
 			this.host = host;
+			hostAssembly = host.GetType ().Assembly;
 
 			codeDomAsmName = typeof (CompilerErrorCollection).Assembly.GetName ();
 			textTemplatingAsmName = typeof (TemplateGenerator).Assembly.GetName ();
+			hostAsmName = hostAssembly.GetName ();
 		}
 
 		protected override Assembly Load (AssemblyName assemblyName)
@@ -38,6 +42,11 @@ namespace Mono.TextTemplating
 			}
 			if (assemblyName.Name == textTemplatingAsmName.Name) {
 				return typeof (TemplateGenerator).Assembly;
+			}
+
+			// the host may be a custom host, and must also be in the same context
+			if (assemblyName.Name == hostAsmName.Name) {
+				return hostAssembly;
 			}
 
 			foreach (var asmFile in templateAssemblyFiles) {
