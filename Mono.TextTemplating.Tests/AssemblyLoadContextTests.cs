de2b3b604438bb3ef48afffd47aaced8a8096ec0
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2022-10-28 17:13:06 -0400

    Fix loading System.Text.Json in ALC
    Fixes #143

--- Mono.TextTemplating.Tests/AssemblyLoadContextTests.cs
+++ Mono.TextTemplating.Tests/AssemblyLoadContextTests.cs
@@ -31,6 +31,21 @@ public class AssemblyLoadContextTests : AssemblyLoadTests<(SnapshotSet<string> a
 
 		state.allContexts.AssertUnchanged ();
 	}
+
+	/// Issue #143: System.Text.Json is a framework assembly on .NET Core 3.0 and does not need to be specified by absolute path
+	[Fact]
+	public async Task LoadSystemTextJson ()
+	{
+		string template = "<#@ assembly name=\"System.Text.Json\" #><#=System.Text.Json.JsonValueKind.Array.ToString()#>";
+
+		var gen = new TemplateGenerator ();
+		(_, string content, _) = await gen.ProcessTemplateAsync (null, template, null);
+
+		CompilerError firstError = gen.Errors.OfType<CompilerError> ().FirstOrDefault ();
+		Assert.Null (firstError);
+
+		Assert.Equal ("Array", content);
+	}
 }
 
 #endif
